3294a2e770570cb80b720e851865f3a0151ce638
Shaike <shaikefatchinaman@gmail.com>
2024-01-22 02:14:24 +0300

    Initial commit

---
+++ src/main/java/ru/inno/lessons/lesson2/lesson.java
@@ -0,0 +1,11 @@
+package ru.inno.lessons.lesson2;
+
+public class lesson {
+    public static void main(String[] args) {
+
+        String test = "test";
+        Boolean testString = test.equals("test");
+
+        System.out.println(testString);
+    }
+}
